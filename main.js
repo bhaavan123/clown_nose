a16b17b4b7e30167c017c15dc827946baa191dfc
bhaavan123 <81519012+bhaavan123@users.noreply.github.com>
2021-12-27 19:30:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+noseX = 0;
+noseY = 0;
+function preload(){
+    clown_nose = loadImage("https://i.postimg.cc/7ZBcjDqp/clownnose.png");
+}
+
+function setup(){
+    canvas = createCanvas(400,400);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(400,400);
+    video.hide();
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+
+}
+
+function gotPoses(results){
+if(results.length>0){
+    console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+console.log("nose x = "+results[0].pose.nose.x);
+console.log("nose y = "+results[0].pose.nose.y);
+}
+
+}
+
+function modelLoaded(){
+    console.log("poseNet is initialized");
+    
+}
+
+function draw(){
+image(video,0,0,400,400);
+image(clown_nose,noseX-15,noseY-15,35,35);
+
+
+}
+
+function take_Snapshot(){
+    save("save_my_picture.png");
+}
